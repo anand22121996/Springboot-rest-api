da8f14a0d36ceca5592df2df1cc162a52c9c449d
Anand Kumar <anand22121996@gmail.com>
2023-03-11 01:18:14 +0530

    used multiple HTTP methods
    Used multiple HTTP methods.

--- src/main/java/com/java/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/java/springbootrestapi/controller/StudentController.java
@@ -3,53 +3,90 @@ package com.java.springbootrestapi.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.java.springbootrestapi.bean.Student;
 
 @RestController
+@RequestMapping("students")
 public class StudentController {
 	
 	//http://localhost:8080/student
 	@GetMapping("student")
-	public Student getStudent() {
+	public ResponseEntity<Student> getStudent() {
 		Student student = new Student(
 				1,
 				"Anand",
 				"Kumar"
 			);
-		return student;
+		//return new ResponseEntity<>(student, HttpStatus.OK);
+		return ResponseEntity.ok(student);
 	}
 	
 	//http://localhost:8080/students
-	@GetMapping("students")
-	public List<Student> getStudents(){
+	@GetMapping
+	public ResponseEntity<List<Student>> getStudents(){
 		List<Student> students = new ArrayList<>();
 		students.add(new Student(1,"Suresh","Kumar"));
 		students.add(new Student(2,"Ramesh","Kumar"));
 		students.add(new Student(3,"Hitesh","Kumar"));
 		
-		return students;
+		return ResponseEntity.ok(students);
 		
 	}
 	
 	//http://localhost:8080/students/1
 	//{id} - URI Template Variable
-	@GetMapping("students/{id}")
-	public Student studentPathVariable(@PathVariable int id) {
-		return new Student(id,"Ramesh","Kumar");
+	@GetMapping("{id}")
+	public ResponseEntity<Student> studentPathVariable(@PathVariable int id) {
+		Student student = new Student(id,"Ramesh","Kumar");
+		return ResponseEntity.ok(student);
 	}
 	
 	//Spring boot REST API with Request Parameter
 	//http://localhost:8080/students/query?id=1&firstName=Ramesh&lastName=Kumar
-	@GetMapping("students/query")
-	public Student studentRequestVariable(@RequestParam int id,
+	@GetMapping("query")
+	public ResponseEntity<Student> studentRequestVariable(@RequestParam int id,
 										  @RequestParam String firstName,
 										  @RequestParam String lastName) {
-		return new Student(id,firstName,lastName);
-
+		Student student =  new Student(id,firstName,lastName);
+		return ResponseEntity.ok(student);
 	}
+	
+	//Spring boot REST API that handles HTTP POST Request to create new resource
+	//@PostMapping and @RequestBody
+    //@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping("/create")
+	public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+		System.out.print(student.getId());
+		System.out.print(student.getFirstName());
+		System.out.print(student.getLastName());
+		return new ResponseEntity<>(student, HttpStatus.CREATED);
+	}
+	
+	//Spring boot REST API that handles HTTP PUT Request - updating existing resource
+	@PutMapping("{id}/update")
+	public ResponseEntity<Student> updateStudent(@RequestBody Student student,@PathVariable("id") int studentId) {
+		System.out.println(student.getFirstName());
+		System.out.println(student.getLastName());
+		return ResponseEntity.ok(student);
+	}
+	
+	//Spring boot REST API that handles HTTP DELETE Request - deleting the existing resource
+	@DeleteMapping("{id}/delete")
+	public ResponseEntity<String> deleteStudent(@PathVariable("id") int studentId) {
+		System.out.println(studentId);
+		return ResponseEntity.ok("Student Deleted Successfully.");
+	}
+	
 }
